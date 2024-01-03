143ee9a2e9eca04db3a6a485a5ffd8f8898c28c8
criscaldeiraa <criscaldeirat@gmail.com>
2024-01-03 16:25:24 +0000

    updating

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import express, { json } from 'express';
-import bcrypt from 'bcrypt-nodejs';
-import cors from 'cors';
-import knex from 'knex';
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
 
-import { handleRegister } from './controllers/register';
-import { handleSignin } from './controllers/signin';
-import { handleProfileGet } from './controllers/profile';
-import { handleImage, handleApiCall } from './controllers/image';
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
@@ -21,14 +21,14 @@ const db = knex({
 const app = express();
 
 app.use(cors())
-app.use(json());
+app.use(express.json());
 
 app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { handleApiCall(req, res)})
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000');
