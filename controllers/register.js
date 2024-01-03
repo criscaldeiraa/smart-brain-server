8cbb210bc286c8b394c06cd8217d9440251b1299
criscaldeiraa <criscaldeirat@gmail.com>
2024-01-03 23:59:42 +0000

    updating

--- controllers/register.js
+++ controllers/register.js
@@ -33,4 +33,4 @@ const handleRegister = (req, res, db, bcrypt) => {
       .catch(err => res.status(400).json('unable to register'))
   }
 
-export default handleRegister;
+export { handleRegister };
