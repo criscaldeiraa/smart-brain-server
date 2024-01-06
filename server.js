0a8c85874dda4ba9b685639f8d4c0e3bb5ec1b15
criscaldeiraa <criscaldeirat@gmail.com>
2024-01-06 03:46:11 +0000

    updating

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
+}).listen(PORT, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
 
