02c781ace0ee1a1406a23c62e1fe3210bf2d8f9a
criscaldeiraa <criscaldeirat@gmail.com>
2024-01-06 15:24:48 +0000

    update

--- server.js
+++ server.js
@@ -30,24 +30,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
-
-(function() {
-  var cors_api_host = 'cors-anywhere.herokuapp.com';
-  var cors_api_url = 'https://' + cors_api_host + '/';
-  var slice = [].slice;
-  var origin = window.location.protocol + '//' + window.location.host;
-  var open = XMLHttpRequest.prototype.open;
-  XMLHttpRequest.prototype.open = function() {
-      var args = slice.call(arguments);
-      var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
-      if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
-          targetOrigin[1] !== cors_api_host) {
-          args[1] = cors_api_url + args[1];
-      }
-      return open.apply(this, args);
-  };
-})();
-
 app.get('/', (req, res) => { res.send(db.users) })
 app.post('/signin', handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) })
