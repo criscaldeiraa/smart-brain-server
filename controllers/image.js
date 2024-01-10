abd906fa81fe9b2d3ca2cf846c4cd02f4a8f28db
criscaldeiraa <criscaldeirat@gmail.com>
2024-01-10 18:18:56 +0000

    clarifai image

--- controllers/image.js
+++ controllers/image.js
@@ -7,32 +7,74 @@ import Clarifai from 'clarifai';
 const clarifai = new Clarifai.App({
   apiKey: process.env.API_CLARIFAI,
 });
-// const PAT = process.env.API_CLARIFAI;
-// const USER_ID = 'clarifai';
-// const APP_ID = 'main';
-// const MODEL_ID = 'face-detection';
-// const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
-// const IMAGE_URL = 'req.body.imageUrl';
+const PAT = process.env.API_CLARIFAI;
+const USER_ID = 'clarifai';
+const APP_ID = 'main';
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
 
-const handleApiCall = async (req, res) => {
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key " + PAT);
+
+const handleApiCall = (req, res) => {
   const { input } = req.body.imageUrl;
-  // if (!input) {
-  //   return res.status(400).json(formatError("please provide image url"));
-  // }
-  try {
-    const { imageUrl } = req.body;
+  const { imageUrl } = req.body;
+  stub.PostModelOutputs(
+    {
+        user_app_id: {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        model_id: MODEL_ID,
+        version_id: MODEL_VERSION_ID, // This is optional. Defaults to the latest model version
+        inputs: [
+            {
+                data: {
+                    image: {
+                        url: IMAGE_URL,
+                        // base64: imageBytes,
+                        allow_duplicate_url: true
+                    }
+                }
+            }
+        ]
+    },
+    metadata,
+    (err, response) => {
+        if (err) {
+            throw new Error(err);
+        }
 
-    const response = await clarifai.models.predict(
-      'c0c0ac362b03416da06ab3fa36fb58e3', // Clarifai's general model for face detection
-      { inputs: [{ data: { image: { url: imageUrl } } }] }
-    );
+        if (response.status.code !== 10000) {
+            throw new Error("Post model outputs failed, status: " + response.status.description);
+        }
 
-    res.json(response);
-  } catch (error) {
-    console.error('Clarifai Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-}
+        const regions = response.outputs[0].data.regions;
+
+        regions.forEach(region => {
+            // Accessing and rounding the bounding box values
+            const boundingBox = region.region_info.bounding_box;
+            const topRow = boundingBox.top_row.toFixed(3);
+            const leftCol = boundingBox.left_col.toFixed(3);
+            const bottomRow = boundingBox.bottom_row.toFixed(3);
+            const rightCol = boundingBox.right_col.toFixed(3);
+
+            region.data.concepts.forEach(concept => {
+                // Accessing and rounding the concept value
+                const name = concept.name;
+                const value = concept.value.toFixed(4);
+
+                console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+
+            })
+          })
+      })
+      .catch(err => res.status(400).json('unable to work with API'));
+};
 
 
 const handleImage = async (req, res, db)  => {
