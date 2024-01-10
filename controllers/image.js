b1c82d42e074c7876dc69330f213d1f9f4b9223f
criscaldeiraa <criscaldeirat@gmail.com>
2024-01-10 13:14:40 +0000

    clarifai image

--- controllers/image.js
+++ controllers/image.js
@@ -6,51 +6,67 @@ import { ClarifaiStub, grpc } from "clarifai-nodejs-grpc";
 
 
 const PAT = process.env.API_CLARIFAI;
-const USER_ID = 'criscaldeiraa';
+const USER_ID = 'clarifai';
 const APP_ID = 'main';
 const MODEL_ID = 'face-detection';
 const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105'
 
+const raw = JSON.stringify({
+  "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+  },
+  "inputs": [
+      {
+          "data": {
+              "image": {
+                  "url": IMAGE_URL
+                  // "base64": IMAGE_BYTES_STRING
+              }
+          }
+      }
+  ]
+});
 
-const stub = ClarifaiStub.grpc();
-
-const metadata = new grpc.Metadata();
-metadata.set("authorization", "Key" + PAT);
+const requestOptions = {
+  method: 'POST',
+  headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+  },
+  body: raw
+};
 
 const handleApiCall = (req, res) => {
-  const { input } = req.body;
+  const { input } = req.body.imageUrl;
   if (!input) {
     return res.status(400).json(formatError("please provide image url"));
   }
-  stub.PostModelOutputs(
-    {
-      user_app_id: {
-          "user_id": USER_ID,
-          "app_id": APP_ID
-      },
-      model_id: MODEL_ID,
-      inputs: [
-          { data: { image: { url: req.body.input, allow_duplicate_url: true } } }
-      ]
-  },
-  metadata,
-  (err, response) => {
-      if (err) {
-        console.log(err);
-        res.status(400).json(formatError("something is wrong"));
-      }
+  fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    .then(response => response.json())
+    .then(result => {
 
-      if (response.status.code !== 10000) {
-        console.log(response.status.description);
-        res.status(400).json(formatError("something is wrong"));
-      }
-      // console.log("Predicted concepts:");
-      // for (const c of response.outputs[0].data.concepts) {
-      //     console.log(c.name + " " + concept.value);
-      // }
-        res.json(response)
-      }
-  )
+        const regions = result.outputs[0].data.regions;
+
+        regions.forEach(region => {
+            // Accessing and rounding the bounding box values
+            const boundingBox = region.region_info.bounding_box;
+            const topRow = boundingBox.top_row.toFixed(3);
+            const leftCol = boundingBox.left_col.toFixed(3);
+            const bottomRow = boundingBox.bottom_row.toFixed(3);
+            const rightCol = boundingBox.right_col.toFixed(3);
+
+            region.data.concepts.forEach(concept => {
+                // Accessing and rounding the concept value
+                const name = concept.name;
+                const value = concept.value.toFixed(4);
+
+                console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+                
+            });
+        });
+})
+.catch(error => console.log('error', error));
 };
 
 const handleImage = async (req, res, db)  => {
